97ea917288265589f9c505f3d10c99f1cc38b4a9
toconuts <toconuts@gmail.com>
2016-06-25 15:55:55 +0700

    Add change password function

---
+++ src/AppBundle/Form/ChangePasswordType.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the TJ-SIF 2016 project.
+ *
+ * (c) toconuts <toconuts@google.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use AppBundle\Form\ChangePasswordType;
+
+/**
+ * Description of ChangePasswordType
+ *
+ * @author toconuts <toconuts@gmail.com>
+ */
+class ChangePasswordType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('oldPassword', PasswordType::class);
+        $builder->add('newPassword', RepeatedType::class, array(
+            'type' => PasswordType::class,
+            'invalid_message' => 'The password fields must match.',
+            'required' => true,
+            'first_options'  => array('label' => 'Password'),
+            'second_options' => array('label' => 'Repeat Password'),
+        ));
+        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Form\Model\ChangePassword',
+        ));
+    }
+}
